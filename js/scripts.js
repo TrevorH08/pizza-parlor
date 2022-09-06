6178e09a4c1e99a151ce9077f91c32addb7b04c5
TrevorH08 <trevorh8700@gmail.com>
2022-09-06 03:15:56 -0700

    add code to pass second test for za

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
+//Business Logic ---------------
+
 function Za(pizzaSize, toppingMeat, toppingVeg) {
   this.size = pizzaSize
   this.meat = toppingMeat
   this.veggie = toppingVeg
+  this.price = 0
 }
+
